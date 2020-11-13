a660a1e0577c00134cadf2535de7630b2817b40a
abhishek_jn <63057914+abhishek-netizen@users.noreply.github.com>
2020-11-13 23:35:53 +0530

    Delete example2.py

--- python programs/example2.py
+++
@@ -1,8 +0,0 @@
-str1 = input("enter the first string:")
-str2 = input("enter the second string:")
-
-if len(str1)>len(str2):
-    print("first string is larger than second string")
-else:
-    print("second string is smaller than first string")
-
